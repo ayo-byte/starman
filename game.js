877ef62c2f727f8dc4513569172d4e4675ed28de
Johanna Gerdts <jg@mavenlist.eu>
2021-11-01 22:27:59 +0100

    added code

---
+++ game.js
@@ -0,0 +1,23 @@
+class Game{
+    setup(){
+        this.player = new Player()
+		this.background = new Background()
+		this.obstacles = []
+    }
+
+//why are set up and constructor separate? why cant set up be in constructor?
+
+    constructor(){
+        this.backgroundImages
+		this.coinImage
+
+    }
+
+    preload(){
+
+    }
+    
+    draw(){
+
+    }
+}
